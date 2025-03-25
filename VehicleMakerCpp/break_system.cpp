15247076f2667f95b037eeefdfbcfa4f3ddcacae
profornnan <profornnan@naver.com>
2025-03-25 16:35:44 +0900

    add: BreakSystem interface 및 class 구현 - BreakSystem interface - Mando class - Continental class - BoschBreak class

---
+++ VehicleMakerCpp/break_system.cpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <string>
+#define interface struct
+
+interface BreakSystem{
+private:
+	std::string name;
+
+protected:
+	BreakSystem(std::string name) : name(name) {}
+
+public:
+	std::string getName() const {
+		return name;
+	}
+};
+
+class Mando : public BreakSystem {
+public:
+	Mando() : BreakSystem("Mando") {}
+};
+
+class Continental : public BreakSystem {
+public:
+	Continental() : BreakSystem("Continental") {}
+};
+
+class BoschBreak : public BreakSystem {
+public:
+	BoschBreak() : BreakSystem("Bosch") {}
+};
+
+// factory pattern
+inline BreakSystem* makeBreakSystem(std::string name) {
+	if (name == "Mando") return new Mando();
+	if (name == "Continental") return new Continental();
+	if (name == "Bosch") return new BoschBreak();
+	return nullptr;
+}
