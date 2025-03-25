810de6c17367e482a45a32769122e033dd7789fe
user <youngwoochoi>
2025-03-25 09:45:14 +0900

    초기 커밋.

---
+++ VehicleMakerUI/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VehicleMakerUI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
