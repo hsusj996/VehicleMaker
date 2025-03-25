52514e25e42f01e235cdff83fef9b27edaa58c2b
sejin <tpwls1355@naver.com>
2025-03-25 14:22:11 +0900

    fix: listener 예외처리 로직 추가 및 쓰레드 의존성 주입 방식 수정
    1. 수신오류 발생 시 다시 listening 상태로 continue
    2. 송신오류 발생 시 무한 재전송
    3. 시스템 오류 발생 시 쓰레드를 종료하고 재귀적으로 다시 실행

--- VehicleMakerCpp/listener.cpp
+++ VehicleMakerCpp/listener.cpp
@@ -1,20 +1,46 @@
 #include "listener.h"
 
 void Listener::startListening() {
-    char data[1024];
-    boost::asio::ip::udp::endpoint sender_endpoint;
+	char data[max_length];
+	boost::asio::ip::udp::endpoint sender_endpoint;
 
-    while (true) {
-        size_t length = socket.receive_from(boost::asio::buffer(data, sizeof(data)), sender_endpoint);
-        std::string response = requestHandler.handle(1);
-        socket.send_to(boost::asio::buffer(response), sender_endpoint);
-    }
+	try {
+		while (listen_flag) {
+			boost::system::error_code ec;
+			size_t length = socket.receive_from(boost::asio::buffer(data, sizeof(data)), sender_endpoint, 0, ec);
+
+			if (ec) {
+				std::cerr << "Error: " << ec.message() << std::endl;
+				continue;
+			}
+
+			if (length > 0) {
+				std::string response = requestHandler.handle(1);
+				socket.send_to(boost::asio::buffer(response), sender_endpoint);
+			}
+			else {
+				continue;
+			}
+
+			std::string response = requestHandler.handle(1);
+			socket.send_to(boost::asio::buffer(response), sender_endpoint);
+		}
+	}
+	catch (const boost::system::system_error& e) {
+		// Boost 시스템 오류
+		// 쓰레드 재실행
+		std::cerr << e.what() << std::endl;
+		startListening();
+	}
+	catch (const std::exception& e) {
+		// 그 외의 오류
+		std::cerr << e.what() << std::endl;
+		// 쓰레드 재실행
+		startListening();
+	}
 }
 
-void listener_thread() {
-    boost::asio::io_context io_context;
-    RequestHandler requestHandler;
-    Listener listener(io_context, 8080, requestHandler);
-
-    listener.startListening();
+void listener_thread(boost::asio::io_context& io_context, RequestHandler& requestHandler, int port) {
+	Listener listener(io_context, port, requestHandler);
+	listener.startListening();
 }
--- VehicleMakerCpp/listener.h
+++ VehicleMakerCpp/listener.h
@@ -2,11 +2,13 @@
 #include "request_handler.h"
 
 class Listener {
+    bool listen_flag = true;                    // 리스닝 플래그 false면 쓰레드 중지
     boost::asio::ip::udp::socket socket;
     boost::asio::ip::udp::endpoint endpoint;
     RequestHandler& requestHandler;
 
 public:
+    static const int max_length = 1024;
     // 생성자에서 socket과 requestHandler를 초기화
     Listener(boost::asio::io_context& io_context, unsigned short port, RequestHandler& requestHandler)
         : socket(io_context, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)), requestHandler(requestHandler) {
