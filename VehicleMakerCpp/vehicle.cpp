6a80f9bdec2b740c89bdcc72da6cd9938b183c94
profornnan <profornnan@naver.com>
2025-03-26 12:45:33 +0900

    feat: add VehicleFactory

--- VehicleMakerCpp/vehicle.cpp
+++ VehicleMakerCpp/vehicle.cpp
@@ -6,7 +6,7 @@
 #define interface struct
 
 interface Vehicle{
-public:
+private:
 	std::string vehicleType;
 	Engine* engine;
 	BreakSystem* breakSystem;
@@ -29,7 +29,13 @@ public:
 	}
 
 	void run() {
-
+		if (engine == nullptr) {
+			std::cout << "엔진이 고장났습니다.\n";
+			std::cout << "자동차가 움직이지 않습니다.\n";
+		}
+		else {
+			std::cout << "자동차가 동작됩니다.\n";
+		}
 	}
 };
 
@@ -49,9 +55,12 @@ public:
 };
 
 // factory pattern
-inline Vehicle* makeVehicle(std::string name) {
-	if (name == "Sedan") return new Sedan();
-	if (name == "Suv") return new Suv();
-	if (name == "Truck") return new Truck();
-	return nullptr;
-}
+class VehicleFactory {
+public:
+	static Vehicle* makeVehicle(std::string name) {
+		if (name == "Sedan") return new Sedan();
+		if (name == "Suv") return new Suv();
+		if (name == "Truck") return new Truck();
+		return nullptr;
+	}
+};
